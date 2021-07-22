5195307235270aca5d65237fa8adf64f7f771647
Mariel Couto <coutomariel@gmail.com>
2021-07-22 15:04:26 -0300

    Ajustes no serviço de cadastro de novas chaves contemplano consulta ao erp e registro da nova chave junto ao bacen

--- key-manager-grpc/src/main/kotlin/br/com/zupacademy/mariel/pix/registra/CadastroNovaChavePixService.kt
+++ key-manager-grpc/src/main/kotlin/br/com/zupacademy/mariel/pix/registra/CadastroNovaChavePixService.kt
@@ -2,6 +2,8 @@ package br.com.zupacademy.mariel.pix.registra
 
 import br.com.zupacademy.mariel.domain.ChavePix
 import br.com.zupacademy.mariel.domain.ChavePixRepository
+import br.com.zupacademy.mariel.pix.integracao.bacen.BacenClient
+import br.com.zupacademy.mariel.pix.integracao.erp.ErpItauClient
 import io.micronaut.validation.Validated
 import org.slf4j.LoggerFactory
 import javax.inject.Inject
@@ -11,18 +13,38 @@ import javax.validation.Valid
 
 @Validated
 @Singleton
-class CadastroNovaChavePixService(@Inject val repository: ChavePixRepository) {
+class CadastroNovaChavePixService(
+    @Inject val repository: ChavePixRepository,
+    @Inject val bacenClient: BacenClient,
+    @Inject val erpItauClient: ErpItauClient
+) {
 
     private val LOGGER = LoggerFactory.getLogger(this::class.java)
 
     @Transactional
-    fun registra(@Valid novaChavePix: NovaChavePixDto) : ChavePix? {
+    fun registra(@Valid novaChavePix: NovaChavePixDto): ChavePix? {
 
         if (repository.existsByChave(novaChavePix.chave)) {
             throw ChavePixExistenteException("Chave Pix ${novaChavePix.chave} existente")
         }
 
-        val registeredPix = repository.save(novaChavePix.toEntity())
+        // Buscar uma conta associada
+        val request = erpItauClient.consultaClientePeloId(novaChavePix.idCliente, novaChavePix.tipoConta)
+        if (request.status.code != 200){
+            throw IllegalStateException("Problema ao buscar uma conta deste tipo com este ID")
+        }
+
+        val detalhesDaConta = request.body()
+        val bacenRequestDto = novaChavePix.toBacenModel(detalhesDaConta)
+
+        // Gravar no bacen
+        val bacenRequest = bacenClient.registra(bacenRequestDto)
+        if (bacenRequest.status.code != 201){
+            LOGGER.info(bacenRequest.status.name)
+            throw IllegalStateException("Problema ao gravar chave no bacen")
+        }
+
+        val registeredPix = repository.save(novaChavePix.toEntity(detalhesContaResponse = detalhesDaConta))
 
         return registeredPix
     }
--- key-manager-grpc/src/main/kotlin/br/com/zupacademy/mariel/pix/registra/NovaChavePixDto.kt
+++ key-manager-grpc/src/main/kotlin/br/com/zupacademy/mariel/pix/registra/NovaChavePixDto.kt
@@ -4,6 +4,11 @@ import br.com.zupacademy.mariel.TipoChave
 import br.com.zupacademy.mariel.TipoConta
 import br.com.zupacademy.mariel.commom.validation.ValidUUID
 import br.com.zupacademy.mariel.domain.ChavePix
+import br.com.zupacademy.mariel.domain.ContaAssociada
+import br.com.zupacademy.mariel.domain.Owner
+import br.com.zupacademy.mariel.pix.integracao.bacen.BanckAccount
+import br.com.zupacademy.mariel.pix.integracao.bacen.ChavePixToRegisterBacenRequest
+import br.com.zupacademy.mariel.pix.integracao.erp.DetalhesContaResponse
 import io.micronaut.core.annotation.Introspected
 import java.util.*
 import javax.validation.constraints.NotBlank
@@ -24,13 +29,31 @@ data class NovaChavePixDto(
     val tipoConta: TipoConta?,
 
     val chave: String
-){
-    fun toEntity() : ChavePix {
+) {
+    fun toEntity(detalhesContaResponse: DetalhesContaResponse): ChavePix {
         return ChavePix(
-            idCliente =  idCliente,
+            idCliente = idCliente,
             tipoChave = tipoChave.toString(),
             tipoConta = tipoConta.toString(),
-            chave = if (tipoChave == TipoChave.CHAVE_ALEATORIA) UUID.randomUUID().toString() else chave
+            chave = if (tipoChave == TipoChave.CHAVE_ALEATORIA) UUID.randomUUID().toString() else chave,
+            contaAssociada = ContaAssociada(detalhesContaResponse.numero, detalhesContaResponse.tipo.toString()),
+            owner = Owner(detalhesContaResponse.titular.nome, detalhesContaResponse.titular.cpf)
         )
+    }
+
+    fun toBacenModel(detalhesDaConta: DetalhesContaResponse): ChavePixToRegisterBacenRequest {
+        return ChavePixToRegisterBacenRequest(
+            keyType = tipoChave!!,
+            key = chave,
+            bankAccount = BanckAccount(
+                participant = detalhesDaConta.titular.nome,
+                accountNumber = detalhesDaConta.numero
+            ),
+            owner = br.com.zupacademy.mariel.pix.integracao.bacen.Owner(
+                name = detalhesDaConta.titular.nome,
+                taxIdNumber = detalhesDaConta.titular.cpf
+            )
+        )
+
     }
 }
